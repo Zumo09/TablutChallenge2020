dadf22f8974b808903a8f4a527aead8fa01aebd5
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2020-11-06 17:23:55 +0100

    good checkpoint

--- Tablut/srcfx/giordani/tabzai/graphics/MainPane.java
+++ Tablut/srcfx/giordani/tabzai/graphics/MainPane.java
@@ -27,42 +27,51 @@ import javafx.scene.layout.HBox;
 public class MainPane extends BorderPane{
 	private TextField folder;
 	private TextField name;
-	private TextField upTo;
-	private TextField separator;
-	private TextField ext;
+	private TextField par;
+	private TextField sep;
+	private TextField game;
 	private TextField out;
 	private ComboBox<String> chooser;
 	private Map<String, Node> charts;
+	private String ext;
 	
 	public MainPane() {
-		name = new TextField();
 		folder = new TextField();
-		ext = new TextField();
-		separator = new TextField();
-		upTo = new TextField();
+		name = new TextField();
+		par = new TextField();
+		sep = new TextField();
+		game = new TextField();
 		out = new TextField();
 		chooser = new ComboBox<>();
 		charts = new HashMap<>();
-		folder.setText("kernels_firtsTry");
-		name.setText("Kernel_");
-		separator.setText("");
-		separator.setPrefColumnCount(1);
-		upTo.setText("50");
-		upTo.setPrefColumnCount(2);
-		ext.setText("");
-		ext.setPrefColumnCount(5);
+		
+		folder.setText("kernels");
+		name.setText("Eclipse");
+		par.setText("4");
+		sep.setText("_");
+		game.setText("5");
+		
+		
+		folder.setPrefColumnCount(10);
+		name.setPrefColumnCount(10);
+		par.setPrefColumnCount(1);
+		sep.setPrefColumnCount(1);
+		game.setPrefColumnCount(2);
+		
+		
 		out.setText("example");
 		out.setPrefColumnCount(20);
 		out.setEditable(false);
+		
 		HBox hb = new HBox();
 		Button b = new Button();
 		b.setText("Print");
 		b.setOnAction(this::print);
 		hb.getChildren().add(folder);
 		hb.getChildren().add(name);
-		hb.getChildren().add(separator);
-		hb.getChildren().add(upTo);
-		hb.getChildren().add(ext);
+		hb.getChildren().add(par);
+		hb.getChildren().add(sep);
+		hb.getChildren().add(game);
 		hb.getChildren().add(b);
 		hb.getChildren().add(out);
 		hb.getChildren().add(chooser);
@@ -70,31 +79,29 @@ public class MainPane extends BorderPane{
 		chooser.setOnAction(e -> {
 			this.setCenter(charts.get(chooser.getSelectionModel().getSelectedItem()));
 		});
+		
+		ext = ".ker";
 	}
 	
 	private void print(ActionEvent e) {
 		charts.clear();
 		ObservableList<String> list = FXCollections.observableArrayList();
 		
-		out.setText(folder.getText() + File.separator + name.getText() + 1 + separator.getText() + 0 + ext.getText());
-		Map<String, List<Double>> data1 = readData(1);
-		Map<String, List<Double>> data2 = readData(2);
-		int i = 0;
-		for(String key : data1.keySet()) {
+		out.setText(folder.getText() + File.separator + name.getText() + par.getText() + sep.getText() + game.getText() + ext);
+		
+		Map<String, List<List<Double>>> data = readAll();
+
+		for(String key : data.keySet()) {
 			NumberAxis ax = new NumberAxis(); ax.setLabel("Generation");
 			NumberAxis ay = new NumberAxis(); ay.setLabel("Value");
-			XYChart.Series<Number, Number> f1 = new XYChart.Series<Number, Number>();
-			XYChart.Series<Number, Number> f2 = new XYChart.Series<Number, Number>();
-			
-			for(int d = 0; d<data1.get(key).size(); d++) {
-				f1.getData().add(new XYChart.Data<>(d+1,data1.get(key).get(d)));
-				f2.getData().add(new XYChart.Data<>(d+1,data2.get(key).get(d)));
-			}
-			
 			LineChart<Number, Number> ch = new LineChart<>(ax, ay);
 			ch.setTitle(key);
-			ch.getData().add(f1);
-			ch.getData().add(f2);
+			for(List<Double> rec : data.get(key)) {
+				XYChart.Series<Number, Number> f = new XYChart.Series<Number, Number>();
+				for(int d = 0; d<rec.size(); d++)
+					f.getData().add(new XYChart.Data<>(d,rec.get(d)));
+				ch.getData().add(f);
+			}
 			charts.put(key, ch);
 			list.add(key);
 		}
@@ -103,17 +110,38 @@ public class MainPane extends BorderPane{
 		chooser.setValue(list.get(0));
 	}
 
-	private Map<String, List<Double>> readData(int n) {
-		Map<String, List<Double>> data = new HashMap<>();
-		int gen = 0;
+	private Map<String, List<List<Double>>> readAll() {
+		Map<String, List<List<Double>>> data = new HashMap<>();
+		int parents = 0;
 		try{
-			gen = Integer.parseInt(upTo.getText());
+			parents = Integer.parseInt(par.getText());
 		} catch(NumberFormatException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		for(int g=1; g<gen; g++) {
-			Path p = Paths.get(folder.getText() + File.separator + name.getText() + n  + separator.getText() + g + ext.getText());
+		
+		for(int n=0; n<parents; n++) {	
+			Map<String, List<Double>> d = readData(n);
+			for(String key : d.keySet()) {
+				if(!data.containsKey(key))
+					data.put(key, new ArrayList<>());
+				data.get(key).add(d.get(key));
+			}
+		}		
+		return data;
+	}
+
+	private Map<String, List<Double>> readData(int n) {
+		Map<String, List<Double>> data = new HashMap<>();
+		int gen = 0;
+		try{
+			gen = Integer.parseInt(game.getText());
+		} catch(NumberFormatException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		for(int g=0; g<gen; g++) {
+			Path p = Paths.get(folder.getText() + File.separator + name.getText() + n + sep.getText() + g + ext);
 			String path = p.toAbsolutePath().toString();
 			try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
 				@SuppressWarnings("unchecked")
