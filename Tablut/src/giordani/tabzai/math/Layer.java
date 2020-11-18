b256aa5f684d427477ce8e9d0ef2f094ca2b6ca4
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2020-11-19 00:40:30 +0100

    Convolution works

--- Tablut/src/giordani/tabzai/math/Layer.java
+++ Tablut/src/giordani/tabzai/math/Layer.java
@@ -18,6 +18,29 @@ public class Layer implements Serializable{
 			layer.add(Matrix.newRandom(rows, cols, ch));
 	}
 	
+	public Matrix get(int index) {
+		return layer.get(index);
+	}
+	
+	public List<Matrix> getList() {
+		return this.layer;
+	}
+	
+	public Matrix convolution(Matrix input) {
+		if(input.getShape(2) != this.get(0).getShape(2) || input.getShape(0) < this.get(0).getShape(0) || input.getShape(1) < this.get(0).getShape(1))
+			throw new IllegalArgumentException("Convolution not applicable for shapes " + input.getShape() + " and " + this.get(0).getShape());
+		int size = input.getShape(0) - this.get(0).getShape(0) + 1;
+		Matrix output = new Matrix(size, size, this.getList().size());
+		for(int f=0; f<this.getList().size(); f++)
+			for(int i=0; i<size; i++)
+				for(int j=0; i<size; i++) {
+					// Perform inner product between filter and a submatrix and assign it to out(i, j, f)
+					Matrix sub = input.getSub(i, j, this.get(f).getShape(0));
+					output.set(i, j, f, this.get(f).inner(sub));
+				}
+		return output;
+	}
+	
 	@Override
 	public String toString() {
 		StringJoiner sj = new StringJoiner("\n]\n[\n", "[\n", "\n]");
--- Tablut/src/giordani/tabzai/math/Matrix.java
+++ Tablut/src/giordani/tabzai/math/Matrix.java
@@ -1,6 +1,7 @@
 package giordani.tabzai.math;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.StringJoiner;
 
@@ -41,7 +42,42 @@ public class Matrix implements Serializable{
 		return shape[c];
 	}
 	
+	public Matrix relu() {
+		for(int i=0; i<this.getShape(0); i++)
+			for(int j=0; j<this.getShape(1); j++)
+				for(int k=0; k<this.getShape(2); k++)
+					if(this.get(i, j, k) < 0)
+						this.set(i, j, k, 0);
+		return this;
+	}
 	
+	public Matrix tanh() {
+		for(int i=0; i<this.getShape(0); i++)
+			for(int j=0; j<this.getShape(1); j++)
+				for(int k=0; k<this.getShape(2); k++)
+					this.set(i, j, k, Math.tanh(this.get(i, j, k)));
+		return this;
+	}
+	
+	public Matrix getSub(int inRow, int inCol, int size) {
+		Matrix sub = new Matrix(size, size, this.getShape(2));
+		for(int i=0; i<size; i++)
+			for(int j=0; j<size; j++)
+				for(int k=0; k<this.getShape(2); k++)
+					sub.set(i, j, k, this.get(i+inRow, j+inCol, k));
+		return sub;
+	}
+	
+	public double inner(Matrix other) {
+		if(!Arrays.equals(this.getShape(), other.getShape()))
+			throw new IllegalArgumentException("Different Shapes");
+		double ret = 0;
+		for(int i=0; i<this.getShape(0); i++)
+			for(int j=0; j<this.getShape(1); j++)
+				for(int k=0; k<this.getShape(2); k++)
+					ret += this.get(i, j, k) * other.get(i, j, k);
+		return ret;
+	}
 
 	@Override
 	public String toString() {
@@ -49,9 +85,9 @@ public class Matrix implements Serializable{
 		for(int i=0; i<this.getShape(0); i++) {
 			StringJoiner sc = new StringJoiner("], [", "[", "]");
 			for(int j=0; j<this.getShape(1); j++) {
-				StringJoiner sh = new StringJoiner(",\t", "[\t", "\t]");
+				StringJoiner sh = new StringJoiner(",\t", "(\t", "\t)");
 				for(int k=0; k<this.getShape(2); k++) {
-					if(k%5 == 0)
+					if(k!=0 && k%5 == 0)
 						sh.add("\n");
 					sh.add(Double.toString(this.get(i, j, k)));
 				}
